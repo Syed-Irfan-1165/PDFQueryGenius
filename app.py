bbc9502937df5922211e3fa0e607c9ceeb6d750b
Syed-Brilyant <syed.irfanuddin@brilyant.com>
2024-05-07 15:37:10 +0530

    splitting text in chunks and converting to vectors

--- app.py
+++ app.py
@@ -25,3 +25,16 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text+=page.extract_text()
     return text
+
+
+## spliiting the PDF text into chunks
+def get_text_chunks(text):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks=text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store=faiss.from_texts(text_chunks,embedding=embeddings)
+    vector_store.save_local("faiss_index")
+    
